1b77c77fdb61aec8adceae530f65830ea576c1e0
VilmantasT <spalviskis@gmail.com>
2018-11-30 15:35:06 +0200

    it works!

--- skaiciuotuvas.js
+++ skaiciuotuvas.js
@@ -2,6 +2,8 @@ var skaicius1 = '';
 var skaicius2 = '';
 var veiksmas = '';
 var lentele = '';
+var atsakymai = [];
+var sortedOrNot = false;
 
 
 
@@ -17,7 +19,6 @@ function addNumber(n) {
 
 function procedura(zodis) {
     veiksmas += zodis;
-    console.log(veiksmas);
 }
 
 function lygybe() {
@@ -32,7 +33,10 @@ function lygybe() {
         atsakymas = Number(skaicius1) / Number(skaicius2);
     }
     document.getElementById('screen').innerHTML = atsakymas;
-    piestiLentele(veiksmas, skaicius1, skaicius2, atsakymas)
+
+    piestiLentele(veiksmas, skaicius1, skaicius2, atsakymas);
+    atsakymai.push([atsakymas, veiksmas, skaicius1, skaicius2]);
+    console.log(atsakymai);
 
     skaicius1 = '';
     skaicius2 = '';
@@ -42,7 +46,7 @@ function lygybe() {
 
 function piestiLentele(v, sk1, sk2, ats) {
     if (lentele.length == 0) {
-        lentele += '<tr><th>A</th><th>Veiksmas</th><th>B</th><th id="sort" onclick="sorting()">=</th></tr>';
+        lentele += '<tr><th>A</th><th>Veiksmas</th><th>B</th><th id="sort" onclick="sorting()">=↑↓</th></tr>';
         lentele += '<tr><td>' + sk1 + '</td><td>' + v + '</td><td>' + sk2 + '</td><td>' + ats + '</td></tr>';
     }else{
         lentele += '<tr><td>' +skaicius1 + '</td><td>' + veiksmas + '</td><td>' + skaicius2 + '</td><td>' + ats + '</td></tr>';
@@ -51,5 +55,41 @@ function piestiLentele(v, sk1, sk2, ats) {
 
 }
 function sorting() {
-    console.log('sorting responsive')
+
+    var skaiciai = [];
+
+    for (var i in atsakymai){
+        skaiciai.push(atsakymai[i][0])
+    }
+
+    if (sortedOrNot) {
+        skaiciai.sort(function (a, b) {
+            return a - b
+        });
+        sortedOrNot = false
+    }else{
+        skaiciai.sort(function (a, b) {
+            return b - a
+        });
+        sortedOrNot = true
+    }
+
+    naujaLentele(skaiciai);
+}
+
+function naujaLentele(array) {
+
+    newlentele = '<tr><th>A</th><th>Veiksmas</th><th>B</th><th id="sort" onclick="sorting()">=↑↓</th></tr>';
+
+
+    for (var i in array) {
+        for (var j in atsakymai){
+            if (array[i] == atsakymai[j][0]) {
+                newlentele += '<tr><td>' + atsakymai[j][2] + '</td><td>' + atsakymai[j][1] + '</td><td>' + atsakymai[j][3] + '</td><td>' + atsakymai[j][0] + '</td></tr>';
+            }
+        }
+    }
+    lentele = newlentele;
+
+    document.getElementById('atsakymas').innerHTML = lentele;
 }
